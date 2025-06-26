1159f288a2675c6794e9fac0a892bea04124b701
catsoup11789 <kirstienkroeger@icloud.com>
2025-06-25 22:34:43 -0500

    revert old random changes to UI library

--- code/src/components/Action/MoreInfo.js
+++ code/src/components/Action/MoreInfo.js
@@ -1,28 +1,31 @@
 import { Button, ButtonText } from '@gluestack-ui/themed';
 import React from 'react';
+import {useColorModeValue, useToken} from 'native-base';
 import { LibrarySystemContext, UserContext, ThemeContext } from '../../context/initialContext';
 
 // custom components and helper files
+import { navigate, navigateStack } from '../../helpers/RootNavigator';
 import {passUserToDiscovery} from '../../util/apiAuth';
 
 export const MoreInfo = (props) => {
-     const { theme, colorMode } = React.useContext(ThemeContext);
-     const { user } = React.useContext(UserContext);
-     const { library } = React.useContext(LibrarySystemContext);
+    const { theme } = React.useContext(ThemeContext);
+    const { user } = React.useContext(UserContext);
+    const { library } = React.useContext(LibrarySystemContext);
 
-     const backgroundColor = colorMode === 'light' ? theme['colors']['warmGray']['200'] : theme['colors']['coolGray']['900'];
-     const textColor = colorMode === 'light' ? theme['colors']['gray']['800'] : theme['colors']['coolGray']['200'];
+    const backgroundColor = useToken('colors', useColorModeValue('warmGray.200', 'coolGray.900'));
+    const textColor = useToken('colors', useColorModeValue('gray.800', 'coolGray.200'));
 
-     return (
-          <Button
-               size="xs"
-               minWidth="100%"
-               maxWidth="100%"
-               bgColor={backgroundColor}
-               onPress={async () => {
-                    await passUserToDiscovery(library.baseUrl, props.module, user.id, backgroundColor, textColor, props.recordId)
-               }}>
-               <ButtonText color={textColor}>{props.title}</ButtonText>
-          </Button>
-     );
+    return (
+        <Button
+            size="xs"
+            minWidth="100%"
+            maxWidth="100%"
+            variant="link"
+            bgColor={backgroundColor}
+            onPress={async () => {
+                passUserToDiscovery(library.baseUrl, props.module, user.id, backgroundColor, textColor, props.recordId)
+            }}>
+            <ButtonText color={textColor}>{props.title}</ButtonText>
+        </Button>
+    );
 };
